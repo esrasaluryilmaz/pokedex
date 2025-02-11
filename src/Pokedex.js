442b559d0381ed82732f9bdc88fcb9bb2cc9280f
esrasaluryilmaz <esrasaluresrasalur@gmail.com>
2025-02-11 01:12:18 +0100

    first commit

---
+++ src/Pokedex.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Pokecard from "./Pokecard";
+import "./Pokedex.css";
+
+class Pokedex extends Component {
+  render() {
+    let title;
+    return (
+      <div className="Pokedex">
+        {title}
+        <h4>Total Experience:{this.props.exp}</h4>
+        <div className="Pokedex-cards">
+          {this.props.pokemon.map((p) => (
+            <Pokecard
+              id={p.id}
+              name={p.name}
+              type={p.type}
+              exp={p.base_experience}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+export default Pokedex;
